4f03507b31ed2f7006fc278044127b9da46a012d
Sandrine Gamman <sandrw14@uia.no>
2022-08-17 14:48:53 +0200

    backend

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -17,11 +17,7 @@ terraform {
   }
 # Terraform State Storage to Azure Storage Container (Values will be taken from Azure DevOps)
   backend "azurerm" {
-    resource_group_name = "terraform-storage-rg"
-    storage_account_name = "terraformstate2011"
-    container_name = "tfstatefiles"
-    key = "dev-terraform.tfstate"
-
+    
   }   
 }
 
