9434ea73dfc3bee7199ffb28353fe10069c3bda7
Sandrine Gamman <sandrw14@uia.no>
2022-08-17 14:43:11 +0200

    ny

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -1,18 +1,18 @@
 # Terraform Block
 terraform {
-  required_version = " latest"
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "lastest" 
+      version = ">= 2.0" 
     }
     random = {
       source = "hashicorp/random"
-      version = "latest"
+      version = ">= 3.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "latest"
+      version = ">= 3.0"
     }    
   }
 # Terraform State Storage to Azure Storage Container (Values will be taken from Azure DevOps)
